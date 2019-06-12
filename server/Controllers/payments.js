02c2564d3ecb04132e07d0f624587c1f63dbe2c5
dakpek <dorukakpek@gmail.com>
2019-06-12 14:55:59 +0300

    iyzipay error

--- server/Controllers/payments.js
+++ server/Controllers/payments.js
@@ -1,40 +1,40 @@
-const iyzipay = require('../IyziPay/iyzipay')
-const user = require('../Databases/users')
+// const iyzipay = require('../IyziPay/iyzipay')
+// const user = require('../Databases/users')
 
-const test = async (ctx, next) => {
+// const test = async (ctx, next) => {
 
-  next()
-  ctx.body = await iyzipay.pay()
-  console.log('Hello', ctx.body)
-}
+//   next()
+//   ctx.body = await iyzipay.pay()
+//   console.log('Hello', ctx.body)
+// }
 
-const pay = async (ctx, next) => {
-  ctx.body = ctx.request.body
-  ctx.body = await iyzipay.pay(ctx.body)
-  next()
-
-  // ctx.body = await iyzipay.pay()
-  // console.log('Hello', ctx.body)
-}
-
-const addCard = async (ctx, next) => {
-  ctx.body = ctx.request.body
-  ctx.body = await iyzipay.addCard(ctx.body)
-  next()
-}
-
-
-
-// const editCatalog = async (ctx, next) => {
+// const pay = async (ctx, next) => {
 //   ctx.body = ctx.request.body
-//   ctx.body = await db.editCatalog(ctx.body)
+//   ctx.body = await iyzipay.pay(ctx.body)
+//   next()
+
+//   // ctx.body = await iyzipay.pay()
+//   // console.log('Hello', ctx.body)
+// }
+
+// const addCard = async (ctx, next) => {
+//   ctx.body = ctx.request.body
+//   ctx.body = await iyzipay.addCard(ctx.body)
 //   next()
 // }
 
 
-//     EXPORTS =================================================================
 
-exports.test = test
-exports.pay = pay
-exports.addCard = addCard
-// exports.getCatalog = getCatalog
+// // const editCatalog = async (ctx, next) => {
+// //   ctx.body = ctx.request.body
+// //   ctx.body = await db.editCatalog(ctx.body)
+// //   next()
+// // }
+
+
+// //     EXPORTS =================================================================
+
+// exports.test = test
+// exports.pay = pay
+// exports.addCard = addCard
+// // exports.getCatalog = getCatalog
